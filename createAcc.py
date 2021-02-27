29a47d87e37a5515288987e22825f0e96f3ae9a6
superdimensional <plusethunder@gmail.com>
2021-02-27 15:20:07 -0500

    started on making the thingy

--- createAcc.py
+++ createAcc.py
@@ -5,17 +5,19 @@ from accCred import passGen
 
 browser = webdriver.Firefox()
 browser.implicitly_wait(5)
-browser.get('https://www.instagram.com/')
+browser.get('https://www.instagram.com/accounts/emailsignup/')
 sleep(2)
 
-username_input = browser.find_element_by_css_selector("input[name='username']")
-password_input = browser.find_element_by_css_selector("input[name='password']")
 
-username_input.send_keys(nameGen())
-password_input.send_keys(passGen())
 
-login_button = browser.find_element_by_xpath("//button[@type='submit']")
-login_button.click()
+username_input = browser.find_element_by_css_selector("input[name='emailOrPhone']")
+# password_input = browser.find_element_by_css_selector("input[name='password']")
 
-sleep(5)
-browser.close()
+username_input.send_keys("testusername")
+# password_input.send_keys(passGen())
+
+# login_button = browser.find_element_by_xpath("//button[@type='submit']")
+# login_button.click()
+
+# sleep(5)
+# browser.close()
