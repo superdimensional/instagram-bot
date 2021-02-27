0520c41baa82253e3b25ed7aad64ac3190d7e388
superdimensional <plusethunder@gmail.com>
2021-02-26 23:19:19 -0500

    is now a module

--- accCred.py
+++ accCred.py
@@ -1,18 +1,13 @@
 import random
-import importlib
-
 
 def numGen():
     n = random.randint(0, 100) # chooses random number between 0 and 100
-    return n * 10
+    n * 10
+    botName = "offworld_agent_" + str(n) # adds the generated number to the end of the name
+    return botName
 
 def passGen():
     n = random.randint(10000000, 1000000000) # chooses a random number between  10 thousand and 1 million
     return n
 #* in the future make a better password thing using actual letters ?
-
-botUsername = "offworld_agent_" + str(numGen()) # adds the generated number to the end of the name
-
-print(botUsername)
-print(passGen())
 
