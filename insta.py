5db4870bda56354c6159b349d40485ca41d47314
superdimensional <plusethunder@gmail.com>
2021-02-26 23:00:34 -0500

    sign in and follow

--- insta.py
+++ insta.py
@@ -1,3 +1,11 @@
 from instapy import InstaPy
 
-InstaPy(username="<your_username>", password="<your_password>").login()
+session = InstaPy(username="tt6965914", password="QeeDzfHMPmZf2g4")
+session.login()
+
+session.set_skip_users(skip_private=False)
+
+#session.like_by_tags(["space", "nasa"], amount=5)
+session.follow_by_list(followlist=['Array_in_a_matrix'], times=1, sleep_delay=600, interact=False)
+session.follow_user_followers(['Array_in_a_matrix'], amount=10, randomize=True, sleep_delay=60)
+session.end()
--- seleniumLogin.py
+++ seleniumLogin.py
@@ -1,35 +1,19 @@
 from time import sleep
-
 from selenium import webdriver
 
-
 browser = webdriver.Firefox()
-
 browser.implicitly_wait(5)
-
-
 browser.get('https://www.instagram.com/')
-
-
 sleep(2)
 
-
 username_input = browser.find_element_by_css_selector("input[name='username']")
-
 password_input = browser.find_element_by_css_selector("input[name='password']")
 
-
 username_input.send_keys("Array_in_a_matrix")
-
 password_input.send_keys("yATM4JG9Jj5NZKQ")
 
-
 login_button = browser.find_element_by_xpath("//button[@type='submit']")
-
 login_button.click()
 
-
 sleep(5)
-
-
 browser.close()
