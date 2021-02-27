6745cb479f1a0c953e87399d60e825bb1b039856
superdimensional <plusethunder@gmail.com>
2021-02-27 15:20:21 -0500

    added a fucking space

--- insta.py
+++ insta.py
@@ -8,4 +8,5 @@ session.set_skip_users(skip_private=False)
 #session.like_by_tags(["space", "nasa"], amount=5)
 session.follow_by_list(followlist=['Array_in_a_matrix'], times=1, sleep_delay=600, interact=False)
 session.follow_user_followers(['Array_in_a_matrix'], amount=10, randomize=True, sleep_delay=60)
+
 session.end()
