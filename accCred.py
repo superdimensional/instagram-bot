ba43ac678c47264359cc95fcfd813c53f9bd5aca
superdimensional <plusethunder@gmail.com>
2021-02-26 23:11:47 -0500

    some things idk

--- accCred.py
+++ accCred.py
@@ -1,17 +1,18 @@
-from time import sleep
-from selenium import webdriver
 import random
+import importlib
 
 
 def numGen():
-    n = random.randint(0, 100)
+    n = random.randint(0, 100) # chooses random number between 0 and 100
     return n * 10
 
 def passGen():
-    n = random.randint(10000000, 1000000000)
+    n = random.randint(10000000, 1000000000) # chooses a random number between  10 thousand and 1 million
     return n
+#* in the future make a better password thing using actual letters ?
 
-botUsername = "offworld_agent_" + str(numGen())
+botUsername = "offworld_agent_" + str(numGen()) # adds the generated number to the end of the name
 
 print(botUsername)
 print(passGen())
+
