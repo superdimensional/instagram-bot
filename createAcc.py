2d6dbfb61c04705fc18a7a9381dc7de6cde1ecef
superdimensional <plusethunder@gmail.com>
2021-02-27 23:18:53 -0500

    fill out html form

--- createAcc.py
+++ createAcc.py
@@ -1,7 +1,9 @@
 from time import sleep
+
 from selenium import webdriver
-from accCred import nameGen
-from accCred import passGen
+from selenium.webdriver.support.ui import Select
+
+from accCred import emailGen, nameGen, passGen
 
 browser = webdriver.Firefox()
 browser.implicitly_wait(5)
@@ -10,14 +12,25 @@ sleep(2)
 
 
 
-username_input = browser.find_element_by_css_selector("input[name='emailOrPhone']")
-# password_input = browser.find_element_by_css_selector("input[name='password']")
+email_input = browser.find_element_by_css_selector("input[name='emailOrPhone']")
+fullname_input = browser.find_element_by_css_selector("input[name='fullName']")
+username_input = browser.find_element_by_css_selector("input[name='username']")
+password_input = browser.find_element_by_css_selector("input[name='password']")
 
-username_input.send_keys("testusername")
-# password_input.send_keys(passGen())
+email_input.send_keys(emailGen())
+fullname_input.send_keys(nameGen())
+username_input.send_keys(nameGen())
+password_input.send_keys(passGen())
 
-# login_button = browser.find_element_by_xpath("//button[@type='submit']")
-# login_button.click()
+login_button = browser.find_element_by_xpath("//button[@type='submit']")
+login_button.click()
 
-# sleep(5)
-# browser.close()
+sleep(3)
+
+selectYear_input = browser.find_element_by_xpath("//select[@title='Year:']")
+selectYear_input.click()
+birthYear_input = browser.find_element_by_xpath("//select[@title='1969']")
+birthYear_input.click()
+
+sleep(10)
+browser.close()
