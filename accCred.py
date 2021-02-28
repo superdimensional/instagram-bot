23d796ddc79f8d3f82b20f095914269555c67236
superdimensional <plusethunder@gmail.com>
2021-02-28 01:06:04 -0500

    changed dash to a dot for email compatibility

--- accCred.py
+++ accCred.py
@@ -4,7 +4,7 @@ import random
 def nameGen():
     n = random.randint(0, 100) # chooses random number between 0 and 100
     n * 10
-    botName = "offworld_agent_" + str(n) # adds the generated number to the end of the name
+    botName = "offworld.agent." + str(n) # adds the generated number to the end of the name
     return botName
 
 def passGen():
