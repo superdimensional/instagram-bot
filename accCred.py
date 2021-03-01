62f9438b06e5c4c34b7bdd388e126124a2e67414
superdimensional <plusethunder@gmail.com>
2021-03-01 18:38:42 -0500

    written shit for outlook

--- accCred.py
+++ accCred.py
@@ -4,12 +4,13 @@ import random
 def nameGen():
     n = random.randint(0, 100) # chooses random number between 0 and 100
     n * 10
-    botName = "offworld.agent." + str(n) # adds the generated number to the end of the name
+    botName = "offworld." + str(n) # adds the generated number to the end of the name
     return botName
 
 def passGen():
     n = random.randint(10000000, 1000000000) # chooses a random number between  10 thousand and 1 million
-    return n
+    passwd = str(n) + "AaAa$"
+    return passwd
 #* in the future make a better password thing using actual letters ?
 
 def emailGen():
