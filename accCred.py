d1fc81168829d39927e87b10c92729689bc936dd
superdimensional <plusethunder@gmail.com>
2021-03-07 22:57:12 -0500

    comments

--- accCred.py
+++ accCred.py
@@ -1,6 +1,7 @@
 import random
 import json
 
+
 def nameGen():
     n = random.randint(0, 1000) # chooses random number between 0 and 100
     n * 10
@@ -11,10 +12,10 @@ def passGen():
     n = random.randint(10000000, 1000000000) # chooses a random number between  10 thousand and 1 million
     passwd = str(n) + "AaAa$"
     return passwd
-#* in the future make a better password thing using actual letters ?
+                                            # TODO: in the future make a better password thing using actual letters ?
 
 def emailGen():                             # takes generated name and makes it an email
-    botMail = toEmail(nameGen())    # TODO: fix inconcistancy, it creates a name for an email that has a diffrent name!
+    botMail = toEmail(nameGen())            # TODO: fix inconcistancy, it creates a name for an email that has a diffrent name!
     return botMail                          # TODO: add an input for the function and pass the name of the account through it
 
 def toEmail(username):
@@ -38,3 +39,29 @@ def credToJSON(username_valid, password_valid):
     })
     with open('credentials/botCred.json', 'a+') as outfile:
         json.dump(credentials, outfile)
+
+def JSONToUsername(entryNumber):
+    input_file = open ('credentials/botCred.json')
+    json_array = json.load(input_file)
+    store_list_username = []
+
+    for item in json_array:
+        stored_username = {"username":None}
+        stored_username['username'] = item['username']
+        store_list_username.append(stored_username)
+
+    username_login = str(store_list_username[entryNumber])
+    return username_login
+
+def JSONToPassword(entryNumber):
+    input_file = open ('credentials/botCred.json')
+    json_array = json.load(input_file)
+    store_list_password = []
+
+    for item in json_array:
+        stored_password = {"password":None}
+        stored_password['password'] = item['password']
+        store_list_password.append(stored_password)
+
+    password_login = str(store_list_password[entryNumber])
+    return password_login
