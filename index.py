7749ced35604ecad8b4ccace1505446cdef57af8
superdimensional <plusethunder@gmail.com>
2021-03-08 01:09:47 -0500

    takes first entry from json and signs in as that

--- index.py
+++ index.py
@@ -1,12 +1,19 @@
 from instapy import InstaPy
 
-session = InstaPy(username="tt6965914", password="QeeDzfHMPmZf2g4")
+from accCred import JSONToUsername, JSONToPassword
+
+
+bot_username = JSONToUsername(0).translate({ord(i): None for i in "{'username': '"}).translate({ord(i): None for i in "'}"})
+bot_password = JSONToPassword(0).translate({ord(i): None for i in "{'password': '"}).translate({ord(i): None for i in "'}"})
+
+
+session = InstaPy(username=bot_username, password=bot_password)
 session.login()
 
 session.set_skip_users(skip_private=False)
 
 #session.like_by_tags(["space", "nasa"], amount=5)
 session.follow_by_list(followlist=['Array_in_a_matrix'], times=1, sleep_delay=600, interact=False)
-session.follow_user_followers(['Array_in_a_matrix'], amount=10, randomize=True, sleep_delay=60)
+# session.follow_user_followers(['Array_in_a_matrix'], amount=10, randomize=True, sleep_delay=60)
 
 session.end()
