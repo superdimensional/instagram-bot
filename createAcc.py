cb64d4bd4c248e00d02564c9543587560bbb5648
superdimensional <plusethunder@gmail.com>
2021-05-12 05:32:50 -0400

    clear up i think

--- createAcc.py
+++ createAcc.py
@@ -20,7 +20,7 @@ password = JSONToPassword(0).translate({ord(i): None for i in "{'password': '"})
 browser = webdriver.Firefox()
 browser.implicitly_wait(5)
 browser.get('https://www.instagram.com/accounts/emailsignup/')
-sleep(2)
+sleep(5)
 
 email_input = browser.find_element_by_css_selector("input[name='emailOrPhone']")
 fullname_input = browser.find_element_by_css_selector("input[name='fullName']")
@@ -34,10 +34,12 @@ fullname_input.send_keys(username)
 username_input.send_keys(username)
 password_input.send_keys(password)
 
+sleep(5)
+
 login_button = browser.find_element_by_xpath('//button[@type="submit"]')            
 login_button.click()                                                               
                                                                                     
-sleep(3)
+sleep(5)
 
 # TODO: add something to check if the username is taken
 # TODO: if username taken, advance 1 entry from the json file and try again
@@ -51,9 +53,9 @@ sleep(3)
 # except NoSuchElementException:
 #     print("Error does not exist\n")
 
-birthMonth = browser.find_element_by_xpath("//select[@title='Month:']") # find select button
-drop = Select(birthMonth)                                               # actully select it
-drop.select_by_value(str(random.randint(1, 12)))                        # choose option based on value
+birthMonth = browser.find_element_by_xpath("//select[@title='Month:']") 
+drop = Select(birthMonth)                                               
+drop.select_by_value(str(random.randint(1, 12)))                        
 
 birthDay = browser.find_element_by_xpath("//select[@title='Day:']")
 drop = Select(birthDay) 
@@ -63,16 +65,23 @@ birthYear = browser.find_element_by_xpath("//select[@title='Year:']")
 drop = Select(birthYear) 
 drop.select_by_value(str(1900 + random.randint(20, 100))) 
 
-# ? i give up
+# ? X button need to be pressed
 
-browser.find_element_by_xpath('//button[@type="button"]').click()
-sleep(2)
-# browser.find_element_by_xpath("//button[@type='button']").click()       # ? this just broke for no reason button literally just stopped working
+# next_button = browser.find_element_by_xpath('//button[@type="button"]')
+# next_button.click()
 
-x_button = browser.find_element_by_class_name('wpO6b  ')#.find_element_by_xpath("//button[@type='button']")
-x_button.click()
+# sleep(10)
+# print("clicking the x button...")
 
-birthYear.send_keys(Keys.TAB, Keys.TAB, Keys.ENTER)
+# x_button = browser.find_element_by_xpath('//button[@class="wpO6b  "]')
+# x_button.click()
 
-# sleep(20)
-# browser.close()
+# # browser.find_element_by_xpath("//button[@type='button']").click()       # ? this just broke for no reason button literally just stopped working
+
+# x_button = browser.find_element_by_class_name('wpO6b  ')#.find_element_by_xpath("//button[@type='button']")
+# x_button.click()
+
+# birthYear.send_keys(Keys.TAB, Keys.TAB, Keys.ENTER)
+
+# # sleep(20)
+# # browser.close()
